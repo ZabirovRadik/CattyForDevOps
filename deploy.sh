6b1eff3468eba23f21af6a72d51f92a9d0d38e2b
Radik Zabirov <zabirovradik070@gmail.com>
2025-11-07 00:16:08 +0400

    last changes

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,16 @@
-﻿#!/bin/bash
-set -e
+﻿set -e
 
 DEPLOY_DIR="/opt/CattyForDevOps"
 MAIN_DIR="/home/ubuntu/DevOPs/DevOps1/CattyForDevOps"
 VENV_PATH="$MAIN_DIR/../venv"
 SERVICE_NAME=catty.service
+
+if [ ! -d "$VENV_PATH" ]; then	
+	python3 -m venv "$VENV_PATH"
+fi
+source "$VENV_PATH/bin/activate"
+pip install --upgrade pip
+pip install -r requirements.txt
 
 # Создаём systemd unit для FastAPI (если нет)
 sudo tee /etc/systemd/system/$SERVICE_NAME > /dev/null <<EOF
--- test.sh
+++ test.sh
@@ -1,5 +1,4 @@
-﻿#!/bin/bash
-set -e
+﻿set -e
 echo "🧪 Running Catty tests..."
 
 MAIN_DIR="/home/ubuntu/DevOPs/DevOps1/CattyForDevOps"
@@ -14,7 +13,7 @@ playwright install --with-deps chromium
 
 # Запускаем приложение в фоне
 echo "🚀 Starting application..."
-$VENV_PATH/bin/uvicorn app.main:app --host 0.0.0.0 --port 8282 &
+$VENV_PATH/bin/uvicorn app.main:app --host 0.0.0.0 --port 8181 &
 APP_PID=$!
 sleep 5
 
@@ -24,14 +23,13 @@ if ! ps -p $APP_PID > /dev/null; then
 fi
 
 
-if ! curl -s http://localhost:8282 > /dev/null; then
-    echo "❌ Application not responding on port 8282"
+if ! curl -s http://localhost:8181 > /dev/null; then
+    echo "❌ Application not responding on port 8181"
     kill $APP_PID
     exit 1
 fi
 
 # Запускаем тесты
-export TEST_BASE_URL="http://localhost:8282"
 pytest -v tests/test_unit.py
 pytest -v tests/test_api.py
 pytest -s -v --browser chromium tests/test_ui.py
